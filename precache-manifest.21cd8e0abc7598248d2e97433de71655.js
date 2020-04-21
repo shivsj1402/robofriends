5934ed79d612b2b77d54147c544a79e56192fda1
Shivkumar Jaiswal <shivsj1402@gmail.com>
2020-04-20 22:19:41 -0300

    Updates

---
+++ precache-manifest.21cd8e0abc7598248d2e97433de71655.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774606a690245b63a4fbbc123b4109f2",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "d8b099112545a09d2291",
+    "url": "/robofriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "0f3222e7b1982596c7c3",
+    "url": "/robofriends/static/css/main.ddaf3f7e.chunk.css"
+  },
+  {
+    "revision": "d8b099112545a09d2291",
+    "url": "/robofriends/static/js/2.4cf2d9f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.4cf2d9f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f3222e7b1982596c7c3",
+    "url": "/robofriends/static/js/main.dd4f8e58.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  }
+]);
